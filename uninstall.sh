40a133db428db5bbf0a68a5b09148ba87be35e99
RezaxOnes <RezaxOneslimited1@gmail.com>
2025-08-12 04:03:11 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -11,10 +11,15 @@ trap 'echo -e "${Red} Khong the thuc hien thao tac do loi khong xac dinh, huy th
 # Thuc thi
 
 echo -e "${Green}[-] Dang go cai dat${RESET}"
-sudo systemctl disable check-battery.timer && sudo systemctl daemon-reexec && sudo systemctl daemon-reload
-sudo rm -rf /etc/systemd/system/check-battery.service
-sudo rm -rf /etc/systemd/system/check-battery.timer
-sudo rm -rf /usr/local/bin/battery-shutdownAT.sh
-sudo rm -rf /var/log/battery-command.log
+echo -e "${Green}[-] Ngung hoat dong chuc nang${RESET}"
+sudo systemctl stop check-battery.timer && sudo systemctl disable check-battery.timer && sudo systemctl daemon-reexec && sudo systemctl daemon-reload
+echo -e "${Green}[-] Tien hanh xoa file${RESET}"
+echo -e "${Green}[-] Dang xoa check-battery.service va check-battery.timer${RESET}"
+sudo rm -f /etc/systemd/system/check-battery.service
+sudo rm -f /etc/systemd/system/check-battery.timer
+echo -e "${Green}[-] Dang xoa battery-shutdownAT.sh${RESET}"
+sudo rm -f /usr/local/bin/battery-shutdownAT.sh
+echo -e "${Green}[-] Dang xoa battery-command.log${RESET}"
+sudo rm -f /var/log/battery-command.log
 
-echo -e "${Green}[-] Da go cai dat thanh cong ${RESET}"
+echo -e "${Green}[-] Da go cai dat thanh cong [-]${RESET}"
