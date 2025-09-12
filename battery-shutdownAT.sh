bd901c53bfa60b8e424e00f9b8aa8fa114390ddc
phat <rezaxonesstudios@gmail.com>
2025-09-13 00:49:26 +0700

    delete hibernate add sync + poweroff instead of poweroff like normal

--- battery-shutdownAT.sh
+++ battery-shutdownAT.sh
@@ -5,11 +5,8 @@ battery=$(cat /sys/class/power_supply/BAT0/capacity)
 if [ "$battery" -lt 7 ]; then
 echo "May Se Tat Nguon Trong 3s do <=7%" >> "$LOGFILE"
 sleep 3
-if ! systemctl hibernate; then
-echo "thuc thi hibernate that bai, tien hanh shutdown - $(date '+%Y-%m-%d %H:%M:%S')" >>  "$LOGFILE" 
-sleep 1
-    systemctl poweroff 
-  fi  
+    sync
+    systemctl poweroff -i  
 else  
 echo "Pin dang lon hon 7 khong thuc thi - $(date '+%Y-%m-%d %H:%M:%S')" >> "$LOGFILE" 
 fi
